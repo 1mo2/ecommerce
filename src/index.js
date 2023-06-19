5fd440663cf97448872e04830e44b84e665c98fe
Mo'men <mo9980716@gmail.com>
2023-06-19 14:12:48 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+// import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css'
+import '@fortawesome/fontawesome-free/js/all.min'
+
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => bootstrap.Tooltip(item))
